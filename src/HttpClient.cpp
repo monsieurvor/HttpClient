93ad360375e860706e2ec3c53981cf8a7b7c3c60
Yvar Dankers <80070435+monsieurvor@users.noreply.github.com>
2024-04-03 23:00:40 +0200

    f

--- src/HttpClient.cpp
+++ src/HttpClient.cpp
@@ -100,7 +100,7 @@ int HttpClient::startRequest(const char* aURLPath, const char* aHttpMethod,
 
     if (iConnectionClose || !iClient->connected())
     {
-        if (!ServerName)
+        if (!iServerName)
         {
             if (!(iClient->connect(iServerName, iServerPort) > 0))
             {
